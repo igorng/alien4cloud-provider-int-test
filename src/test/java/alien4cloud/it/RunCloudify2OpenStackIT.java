7f75bdebd6e86fbcbd518929d63cbfc30b190ca5
vuminhkh <minh.khang.vu.mkv@gmail.com>
2015-07-20 10:32:54 +0200

    Fix tests cfy2

--- src/test/java/alien4cloud/it/RunCloudify2OpenStackIT.java
+++ src/test/java/alien4cloud/it/RunCloudify2OpenStackIT.java
@@ -1,6 +1,5 @@
 package alien4cloud.it;
 
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
@@ -9,6 +8,5 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "classpath:features/cloudify2/openstack/" }, format = { "pretty", "html:target/cucumber/cloudify2/openstack",
         "json:target/cucumber/cloudify2/cucumber-openstack.json" })
-@Ignore
 public class RunCloudify2OpenStackIT {
 }
