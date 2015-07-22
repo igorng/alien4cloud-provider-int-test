585e73c4e6bbe5eddd1219fe65db98e053df8b91
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2015-07-22 17:06:36 +0200

    for merge

--- src/test/java/alien4cloud/it/RunCloudify2OpenStackIT.java
+++ src/test/java/alien4cloud/it/RunCloudify2OpenStackIT.java
@@ -1,14 +1,16 @@
 package alien4cloud.it;
 
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "classpath:features/cloudify2/openstack/" }, format = { "pretty", "html:target/cucumber/cloudify2/openstack",
-        "json:target/cucumber/cloudify2/cucumber-openstack.json" })
-@Ignore
+@CucumberOptions(features = {
+// "classpath:features/cloudify2/openstack/"
+"classpath:features/cloudify2/openstack/multi_storages.feature"
+
+}, format = { "pretty", "html:target/cucumber/cloudify2/openstack", "json:target/cucumber/cloudify2/cucumber-openstack.json" })
+// @Ignore
 public class RunCloudify2OpenStackIT {
 }
