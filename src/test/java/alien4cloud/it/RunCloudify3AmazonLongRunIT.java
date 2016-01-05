0274c6773277fb35fc80dbc293587181b96a654b
Philippe Benoit <philippe.benoit@fastconnect.fr>
2016-01-05 12:57:52 +0100

    fixup! Fix Path in long run tests

--- src/test/java/alien4cloud/it/RunCloudify3AmazonLongRunIT.java
+++ src/test/java/alien4cloud/it/RunCloudify3AmazonLongRunIT.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "classpath:features/cloudify3/amazon_longrun" }, format = { "pretty", "html:target/cucumber/cloudify3/amazon_longrun",
+@CucumberOptions(features = { "classpath:features/cloudify3/longruntest/amazon_longrun" }, format = { "pretty", "html:target/cucumber/cloudify3/amazon_longrun",
         "json:target/cucumber/cloudify3/cucumber-amazon_longrun.json" })
 public class RunCloudify3AmazonLongRunIT {
 }
--- src/test/java/alien4cloud/it/RunCloudify3OpenStackLongRunIT.java
+++ src/test/java/alien4cloud/it/RunCloudify3OpenStackLongRunIT.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "classpath:features/cloudify3/openstack_longrun" }, format = { "pretty", "html:target/cucumber/cloudify3/openstack_longrun",
+@CucumberOptions(features = { "classpath:features/cloudify3/longruntest/openstack_longrun" }, format = { "pretty", "html:target/cucumber/cloudify3/openstack_longrun",
         "json:target/cucumber/cloudify3/cucumber-openstack_longrun.json" })
 public class RunCloudify3OpenStackLongRunIT {
 }
